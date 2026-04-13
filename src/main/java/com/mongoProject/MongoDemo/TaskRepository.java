b54cfc4568a5d22cba96d5f71642cb43d2e65d2f
Abhishek <meabhibarawal@gmail.com>
2026-04-13 08:02:46 +0530

    initial commit

---
+++ src/main/java/com/mongoProject/MongoDemo/TaskRepository.java
@@ -0,0 +1,15 @@
+package com.mongoProject.MongoDemo;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface TaskRepository extends MongoRepository<Task,String> {
+
+    List<Task> findBySeverity(int severity);
+
+    @Query("{ assignee: ?0 }")
+    List<Task> getTasksByAssignee(String assignee);
+}
---
+++ src/test/java/com/mongoProject/MongoDemo/MongoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongoProject.MongoDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
